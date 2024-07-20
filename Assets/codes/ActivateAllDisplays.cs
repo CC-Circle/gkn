1e2c4686437976d463ffa148dcf8c27be524582e
shuta1123 <shutay1123@yahoo.co.jp>
2024-07-20 23:53:06 +0900

    ２回目以降のバグ修正

--- Assets/codes/ActivateAllDisplays.cs
+++ Assets/codes/ActivateAllDisplays.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ActivateAllDisplays : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -20,6 +20,6 @@ public class ActivateAllDisplays : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SceneManager.LoadScene("Title");
     }
 }
